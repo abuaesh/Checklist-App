c31d3540a2eee8df1255e4a5fe839e4ce157a992
Noha Abuaesh <noha.abuaesh@gmail.com>
2021-03-28 21:24:18 +0300

    Fetch todo list from postgres

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://postgres:12345@localhost:5432/todoapp'
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo id={self.id} description={self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {'description': 'Todo 1'},
-        {'description': 'Todo 2'},
-        {'description': 'Todo 3'}
-    ])
+    return render_template('index.html', data=Todo.query.all())
+
+@app.route('/create', methods=['POST'])
+def create():
+    new_description = request.form.get('description')
+    db.session.add(Todo(description=new_description))
+    db.session.commit()
